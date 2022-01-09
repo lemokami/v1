21817d3f8466b5da69ef1a9eacae91bd44e78b3d
lemokami <jesswinw@gmail.com>
2022-01-09 22:48:58 +0530

    feat: add an under construction message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="h-screen  flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl">🚧The site is under construction🚧</h1>
+      <span className="">
+        Go to my 👉🏻{" "}
+        <a
+          className="text-blue-600 underline hover:text-red-500 transition-colors"
+          href="https://github.com/lemokami"
+        >
+          Github
+        </a>
+        👈🏻 to find out more about me.
+      </span>
+    </div>
+  );
 }
