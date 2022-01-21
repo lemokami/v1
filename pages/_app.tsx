92febb66ab97b0bff47f346825626aedb14aa74f
lemokami <jesswinw@gmail.com>
2022-01-21 17:00:49 +0530

    feat: framer motion context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AnimateSharedLayout>
+      <AnimatePresence>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </AnimateSharedLayout>
+  );
 }
 
 export default MyApp;
