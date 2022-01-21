db7a8abfce5e4172282f6a5eb6116fae82dfe2a0
lemokami <jesswinw@gmail.com>
2022-01-21 16:58:23 +0530

    feat: installed framer-motion, react-icons and zustand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,32 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D1D1D",
+        secondary: "#4BC583",
+        white: "#FAFAFA",
+        gray: "#C4C4C4",
+        "secondary-light": "EBFFF4",
+      },
+      fontFamily: {
+        primary: ["Galderglynn Titling Rg", "serif"],
+        secondary: ["Poppins", "sans-serif"],
+      },
+    },
+    letterSpacing: {
+      tightest: "-.075em",
+      tighter: "-.05em",
+      tight: "-.025em",
+      normal: "0",
+      wide: ".025em",
+      wider: ".05em",
+      widest: ".1em",
+      widest: ".25em",
+    },
   },
   plugins: [],
 };
