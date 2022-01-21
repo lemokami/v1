0214fa3406d64cd35d87614201ae04e5aba544cb
lemokami <jesswinw@gmail.com>
2022-01-21 20:47:01 +0530

    feat: add animation to top text

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useRouter } from "next/router";
 import { FC } from "react";
 import Sidebar from "./Sidebar";
@@ -7,11 +8,13 @@ const Layout: FC = ({ children }) => {
   console.log(router.pathname);
   return (
     <div className="grid grid-cols-9 h-screen relative">
-      <img
+      <motion.img
         src={`/decorators/${
           router.pathname == "/" ? "home" : router.pathname
         }.svg`}
         className="absolute -top-10 -left-10 h-40 opacity-50 pointer-events-none"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
       />
       <Sidebar className="col-span-1 border-r-2 border-gray" />
       <div className="col-span-8">{children}</div>
