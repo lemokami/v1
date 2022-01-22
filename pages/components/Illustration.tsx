e99a80356722dfcf4133cb2ef9cce8de7c63d3e3
lemokami <jesswinw@gmail.com>
2022-01-22 20:51:54 +0530

    feat: illustration component that handle the right illustration

---
+++ pages/components/Illustration.tsx
@@ -0,0 +1,31 @@
+import { motion, Variants } from "framer-motion";
+
+const illustration: Variants = {
+  hidden: { scale: 0.5 },
+  show: { scale: 1 },
+};
+
+const Illustration = () => {
+  return (
+    <>
+      {/* <motion.img
+        src="/assets/blob.svg"
+        className="h-screen absolute -right-56 -bottom-16"
+        alt="me"
+        variants={illustration}
+        initial="hidden"
+        animate="show"
+      /> */}
+      <motion.img
+        src="/assets/potrait.svg"
+        className="h-screen absolute -right-32 -bottom-28 -z-10"
+        alt="me"
+        variants={illustration}
+        initial="hidden"
+        animate="show"
+      />
+    </>
+  );
+};
+
+export default Illustration;
