45c8f42688ed0d90edd2c662a6da95c0f6579030
lemokami <jesswinw@gmail.com>
2022-01-22 20:43:31 +0530

    feat: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
+import Illustration from "./components/Illustration";
 import Layout from "./components/Layout";
+import SocialBar from "./components/SocialBar";
 
 export default function Home() {
-  const [show, setShow] = useState(true);
-
   return (
     <Layout>
-      <div className="h-full flex flex-col items-center justify-center font-primary">
-        Hello
+      <div className="h-full flex flex-col font-primary items-start justify-center font-bold text-primary">
+        <div className="z-50">
+          <span className="tracking-spaced text-2xl">私はレモカミです</span>
+          <h1 className="text-8xl tracking-wider capitalize">
+            jesswin <span className="text-secondary">W</span>
+          </h1>
+          <h1 className="text-8xl tracking-wider capitalize">varghese</h1>
+        </div>
+        <Illustration />
+        <SocialBar />
       </div>
     </Layout>
   );
