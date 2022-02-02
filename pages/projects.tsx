4f33358203862a4ea24b3dc9525828c14a9d4352
lemokami <jesswinw@gmail.com>
2022-02-02 17:27:22 +0530

    feat: add social bar to the page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,5 @@
 import Layout from "./components/Layout";
+import SocialBar from "./components/SocialBar";
 
 type Props = {};
 
@@ -8,6 +9,7 @@ export default function Projects({}: Props) {
       <div className="h-full flex flex-col items-center justify-center font-primary">
         Projects
       </div>
+      <SocialBar />
     </Layout>
   );
 }
